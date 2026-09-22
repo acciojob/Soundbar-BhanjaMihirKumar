f202a838906ee0c449fae0ed778c88d0f7bca974
Mihir kumar Bhanja <139785978+BhanjaMihirKumar@users.noreply.github.com>
2026-09-23 00:38:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+const buttons = document.querySelectorAll(".btn");
+const stopButton = document.querySelector(".stop");
+
+let currentAudio = null;
+
+buttons.forEach(button => {
+    button.addEventListener("click", () => {
+
+        // Stop currently playing sound
+        if (currentAudio) {
+            currentAudio.pause();
+            currentAudio.currentTime = 0;
+        }
+
+        // Get sound name from button
+        let soundName = button.innerText;
+
+        // Create audio
+        currentAudio = new Audio(`sounds/${soundName}.mp3`);
+
+        // Play sound
+        currentAudio.play();
+    });
+});
+
+stopButton.addEventListener("click", () => {
+    if (currentAudio) {
+        currentAudio.pause();
+        currentAudio.currentTime = 0;
+    }
+});
